b4739ca1d1dcd8b2fc58b539e7e1f5377c340323
darceym <darcey.meus@gmail.com>
2019-12-14 22:14:37 -0500

    application is able to view employees in a table in node

--- index.js
+++ index.js
@@ -21,41 +21,101 @@ const addDepartment = () => {
 
 const addRole = () => {
     connection.query("SELECT * FROM departments", (err, rows) => {
-      if (err) throw err;              
-    
-      inquirer.prompt ([
-        {
-            type: "input",
-            message: "Title?", 
-            name: "title" 
-        },
-        {
-            type: "input",
-            message: "Salary?",  
-            name: "salary"
-        },
-        {
-            type: "list",
-            message: "Department?", 
-            name: "department", 
-            choices: function() {
-                return rows.map(row =>{
-                    return { name: row.name, value: row.id, short: row.name };
-                })
+        if (err) throw err;
+
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "Title?",
+                name: "title"
+            },
+            {
+                type: "input",
+                message: "Salary?",
+                name: "salary"
+            },
+            {
+                type: "list",
+                message: "Department?",
+                name: "department",
+                choices: function () {
+                    return rows.map(row => {
+                        return { name: row.name, value: row.id, short: row.name };
+                    })
+                }
             }
-        }
-    ]).then(answers => {
-        connection.query("INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)", 
-        [answers.title, answers.salary, answers.department], (err, data) => {
-            if (err) throw err;
-            mainMenu();
+        ]).then(answers => {
+            connection.query("INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)",
+                [answers.title, answers.salary, answers.department], (err, data) => {
+                    if (err) throw err;
+                    mainMenu();
+                });
         });
-    }); 
-  });
+    });
 };
 
 const addEmployee = () => {
-    console.log("Adding Employee");
+    connection.query("SELECT * FROM roles", (err, roles) => {
+        if (err) throw err;
+        connection.query(" SELECT * FROM employees", (err, emloyees) => {
+
+            if (err) throw err;
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: "First name?",
+                    name: "firstName"
+                },
+                {
+                    type: "input",
+                    message: "Last name?",
+                    name: "lastName"
+                },
+                {
+                    type: "list",
+                    message: "Role?",
+                    name: "roleId",
+                    choices: function () {
+                        return roles.map(role => {
+                            return { name: role.title, value: role.id, short: role.title };
+                        });
+                    }
+                },
+                {
+                    type: "list",
+                    message: "Manager",
+                    name: "managerId",
+                    choices: function () {
+                        return employees.map(emp => {
+                            return { name: `${emp.first_name} ${emp.last_name}`, value: emp.id, short: emp.last_name };
+                        });
+                    }
+                }
+            ]).then(answers => {
+                connection.query("INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)",
+                    [answers.firstName, answers.lastName, answers.roleId, answers.managerId],
+                    (err, results) => {
+                        if (err) throw err;
+                        mainMenu();
+                    }
+                );
+            });
+        });
+    });
+
+};
+
+const viewEmployees = () => {
+    connection.query(`SELECT emp.first_name, emp.last_name, r.title AS role_title, CONCAT (mgr.first_name, " " mgr.last_name) AS managers_name
+     FROM employees AS emp
+     LEFT JOIN roles AS r ON emp.role_id = r.id
+     LEFT JOIN employees AS mgr ON emp.manager_id = mgr.id
+     LEFT JOIN departments AS d ON r.department_id = d.id`,
+        (err, results) => {
+            console.table(results);
+
+            mainMenu();
+        });
 };
 
 const mainMenu = () => {
@@ -67,7 +127,8 @@ const mainMenu = () => {
             choices: [
                 "Add Department",
                 "Add Role",
-                "Add Employee"
+                "Add Employee",
+                "View Employees"
             ]
         }
     ]).then(answers => {
@@ -81,8 +142,12 @@ const mainMenu = () => {
             case "Add Employee":
                 addEmployee();
                 break;
+            case "View Employees":
+                viewEmployees();
+                break;
             default:
                 console.log("Exiting...")
+                connection.end();
         }
     });
 };
