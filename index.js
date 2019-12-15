c32c688b11f84386dc4ec9051af3c641ed1eb8ce
darceym <darcey.meus@gmail.com>
2019-12-14 21:17:09 -0500

    application is able to add departments and add roles to database

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 
-const insertDepartment = name => {
-    
-}
 
 const addDepartment = () => {
     inquirer.prompt([
@@ -13,40 +10,94 @@ const addDepartment = () => {
             message: "Department name?"
         }
     ]).then(answers => {
-        insertDepartment(answers.name);
+        connection.query("INSERT INTO departments (name) VALUES (?)", [answers.name], (err, rows) => {
+            if (err) throw err;
+            console.log(`Added ${answers.name} to departments.`);
+            mainMenu();
+        });
     })
     console.log("Adding Dept");
 };
+
 const addRole = () => {
-    console.log("Adding Role");
+    connection.query("SELECT * FROM departments", (err, rows) => {
+      if (err) throw err;              
+    
+      inquirer.prompt ([
+        {
+            type: "input",
+            message: "Title?", 
+            name: "title" 
+        },
+        {
+            type: "input",
+            message: "Salary?",  
+            name: "salary"
+        },
+        {
+            type: "list",
+            message: "Department?", 
+            name: "department", 
+            choices: function() {
+                return rows.map(row =>{
+                    return { name: row.name, value: row.id, short: row.name };
+                })
+            }
+        }
+    ]).then(answers => {
+        connection.query("INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)", 
+        [answers.title, answers.salary, answers.department], (err, data) => {
+            if (err) throw err;
+            mainMenu();
+        });
+    }); 
+  });
 };
+
 const addEmployee = () => {
     console.log("Adding Employee");
 };
 
-inquirer.prompt([
-    {
-        type: "list",
-        name: "action",
-        message: "What would you like to do?",
-        choices: [
-            "Add Department",
-            "Add Role",
-            "Add Employee"
-        ]
-    }
-]).then(answers => {
-    switch (answers.action) {
-        case "Add Department":
-            addDepartment();
-            break;
-        case "Add Role":
-            addRole();
-            break;
-        case "Add Employee":
-            addEmployee();
-            break;
-        default:
-            console.log("Exiting...")
-    }
-})
+const mainMenu = () => {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "action",
+            message: "What would you like to do?",
+            choices: [
+                "Add Department",
+                "Add Role",
+                "Add Employee"
+            ]
+        }
+    ]).then(answers => {
+        switch (answers.action) {
+            case "Add Department":
+                addDepartment();
+                break;
+            case "Add Role":
+                addRole();
+                break;
+            case "Add Employee":
+                addEmployee();
+                break;
+            default:
+                console.log("Exiting...")
+        }
+    });
+};
+
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "rootroot",
+    database: "employee_tracker"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+
+    mainMenu();
+});
